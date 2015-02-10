9a90a5696df041f0883aa3391f205978e3b43d30
Hanne <hannekottum@gmail.com>
2015-02-10 15:53:01 +0100

    Live workable search autocomplete in view2

--- test/app/view2/view2.js
+++ test/app/view2/view2.js
@@ -9,26 +9,17 @@ angular.module('myApp.view2', ['ngRoute', 'ui.bootstrap'])
   });
 }])
     .controller('View2Ctrl', function($scope){
-        $scope.selected = undefined;
+        $scope.testing = [];
         $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', '' +
         'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
             'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
             'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
             'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
             'West Virginia', 'Wisconsin', 'Wyoming'];
-        // Any function returning a promise object can be used to load values asynchronously
-        $scope.getLocation = function(val) {
-            return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
-                params: {
-                    address: val,
-                    sensor: false
-                }
-            }).then(function(response){
-                return response.data.results.map(function(item){
-                    return item.formatted_address;
-                });
-            });
+        $scope.onSelect = function ($item) {
+            $scope.testing.push($item);
         };
+
 
 
 
